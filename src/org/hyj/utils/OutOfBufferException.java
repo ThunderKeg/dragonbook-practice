cbae6e463235546ac8eba81a272e0371e81aa424
ThunderKeg <huangyj1989@163.com>
2014-10-05 12:48:28 +0800

    Revert "Revert "create the buffer""
    This reverts commit 7682b6693fad2fd16b055408fd91ae8bc80ca968.

---
+++ src/org/hyj/utils/OutOfBufferException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.hyj.utils;
+
+/**
+ * @author Yujie Huang
+ *
+ */
+public class OutOfBufferException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -398233529933927741L;
+
+}
